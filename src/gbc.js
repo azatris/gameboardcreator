b7149bff8c0b0b9639e075a730d2b87f926ac2b0
Silver Taza <azatris@gmail.com>
2014-01-20 15:35:15 +0800

    Basic functionality done

---
+++ src/gbc.js
@@ -0,0 +1,58 @@
+var tileWidth = 117;
+var tileHeight = 135;
+
+var w = window.innerWidth;
+var h = window.innerHeight;
+
+var stage = new Kinetic.Stage({
+  container: 'container',
+  x: w*0.03,
+  y: h*0.03,
+  width: w*0.97,
+  height: h*0.97
+});
+var layer = new Kinetic.Layer();
+
+var boardSizeX = parseInt(prompt("Please insert the horizontal number of tiles:","24"));
+var boardSizeY = parseInt(prompt("Please insert the vertical number of tiles:","8"));
+
+// var boardSizeX = 12;
+// var boardSizeY = 36;
+var scaleX = (w*0.9/tileWidth - 0.5)/boardSizeX;
+var scaleY = (h*0.9/(tileHeight*0.75) - 0.25)/boardSizeY;
+var scale = Math.min(scaleX, scaleY);
+console.log("scaleX: " + scaleX + " and scaleY: " + scaleY);
+
+
+for (var i = 0; i < boardSizeY; i++) {
+  for (var j = 0; j < boardSizeX; j++) {
+    var x = i % 2 == 0 ? tileWidth*j*scale : (tileWidth*j + tileWidth/2)*scale;
+    var y = tileHeight*0.75*i*scale;
+    drawTileAt(x, y);  
+  }
+}
+
+
+console.log(stage[x]);
+
+function drawTileAt(x, y) {
+  var tile = new Image();
+  tile.onload = function() {
+    var aTile = new Kinetic.Image({
+      x: x,
+      y: y,
+      image: tile,
+      width: 117,
+      height: 135
+    });
+    aTile.scale({x:scale,y:scale});
+    layer.add(aTile);
+    stage.add(layer);
+  };
+  if (Math.random() > 0.2) {
+    tile.src = "bin/tile_sea.png";
+  } else {
+    tile.src = "bin/tile_land.png";
+  }
+
+}
